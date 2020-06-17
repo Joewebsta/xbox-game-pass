35f7e62f87fe747b33c21cf8e36e3b8c7e9eff20
Joe Webster <joseph.scott.webster@gmail.com>
2020-06-17 13:50:57 -0400

    Add release date game attribute

--- scripts.js
+++ scripts.js
@@ -7,12 +7,14 @@ function scrapeGamesHTML(array) {
     const imgURL = game.querySelector('img.c-image').src;
     const multiplayer = game.dataset.multiplayer;
     const platforms = game.querySelector('span.c-glyph').getAttribute('aria-label');
-    
+    const releaseDate = parseInt(game.dataset.releasedate.slice(0,4));
+
     return {
       name,
       imgURL,
       multiplayer,
       platforms,
+      releaseDate,
     }
   });
 }
@@ -20,22 +22,6 @@ function scrapeGamesHTML(array) {
 const sanitizedGames = scrapeGamesHTML(gamesArray);
 
 console.log(sanitizedGames);
-
-
-// [
-//   {
-//     name: "DayZ",
-//     imageUrl: "",
-//     multiplayer: false,
-//     platforms: ["console"],
-//   }, 
-// ];
-
-
-//Name
-//Image URL
-//Multiplayer
-//Availabile on
 
 //Rating - LATER
 //Categorty - LATER
