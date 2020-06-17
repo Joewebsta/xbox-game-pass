bf1083425040fff9406793c5f7919902217bcb6d
Joe Webster <joseph.scott.webster@gmail.com>
2020-06-17 11:44:35 -0400

    Add xbox libary html and access useful game attributes via JS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-console.log("It works!");
+const games = document.querySelectorAll('.m-product-placement-item');
+const gamesArray = Array.from(games);
+gamesArray.forEach(game => {
+  const name = game.querySelector('.x1GameName').textContent;
+  const imgURL = game.querySelector('img.c-image').src;
+  const multiplayer = game.dataset.multiplayer;
+  const platforms = game.querySelector('span.c-glyph').getAttribute('aria-label');
+  console.log(name, platforms)
+});
+
+
+//Name
+//Image URL
+//Multiplayer
+//Availabile on
+
+//Rating - LATER
+//Categorty - LATER
