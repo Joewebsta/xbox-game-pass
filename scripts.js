b4add2e3acac418c25d62cac304b663e1ab073b6
Joe Webster <joseph.scott.webster@gmail.com>
2020-06-17 12:47:37 -0400

    Create scrapeGamesHTML function and return array of games objects

--- scripts.js
+++ scripts.js
@@ -1,12 +1,35 @@
 const games = document.querySelectorAll('.m-product-placement-item');
 const gamesArray = Array.from(games);
-gamesArray.forEach(game => {
-  const name = game.querySelector('.x1GameName').textContent;
-  const imgURL = game.querySelector('img.c-image').src;
-  const multiplayer = game.dataset.multiplayer;
-  const platforms = game.querySelector('span.c-glyph').getAttribute('aria-label');
-  console.log(name, platforms)
-});
+
+function scrapeGamesHTML(array) {
+  return array.map(game => {
+    const name = game.querySelector('.x1GameName').textContent;
+    const imgURL = game.querySelector('img.c-image').src;
+    const multiplayer = game.dataset.multiplayer;
+    const platforms = game.querySelector('span.c-glyph').getAttribute('aria-label');
+    
+    return {
+      name,
+      imgURL,
+      multiplayer,
+      platforms,
+    }
+  });
+}
+
+const sanitizedGames = scrapeGamesHTML(gamesArray);
+
+console.log(sanitizedGames);
+
+
+// [
+//   {
+//     name: "DayZ",
+//     imageUrl: "",
+//     multiplayer: false,
+//     platforms: ["console"],
+//   }, 
+// ];
 
 
 //Name
