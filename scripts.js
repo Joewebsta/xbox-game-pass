ca290cd767a9ea18e8e7aa39fc4f6b1afc3b55c5
Joe Webster <joseph.scott.webster@gmail.com>
2020-07-09 13:35:04 -0400

    Refactor game sorting functionality

--- scripts.js
+++ scripts.js
@@ -1,71 +1,59 @@
-fetch('games.json').then((response) => { 
+fetch('games.json').then((response) => {
   return response.json();
 }).then((json) => {
-  games = json;
-  initialize(games);
+  initialize(json);
 }).catch((err) => {
-  console.log('Fetch problem: ' + err.message); 
+  console.log('Fetch problem: ' + err.message);
 });
 
 function initialize(games) {
-  displayGames(sortGamesByName(games));
+  const formContainer = document.querySelector('.form-container');
+  const gamesContainer = document.querySelector('.games-container');
+  const sortInput = document.querySelector('#sort');
+
+  sortGames(games);
+
+  function sortGames(games) {
+    const sortType = sortInput.value;
+
+    const sortedGames = games.sort((a, b) => {
+      if (sortType === 'nameA') return a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1;
+      if (sortType === 'nameD') return a.name.toUpperCase() < b.name.toUpperCase() ? 1 : -1;
+      if (sortType === 'releaseDateN') return a.releaseDate < b.releaseDate ? 1 : -1;
+      if (sortType === 'releaseDateO') return a.releaseDate < b.releaseDate ? -1 : 1;
+    })
+
+    displayGames(sortedGames);
+  }
+
+  function displayGames(games) {
+    gamesContainer.textContent = "";
+
+    games.forEach(game => {
+      const gameContent = `
+      <div class="game-container">
+      <img src="${game.imgURL}">
+      <div class="rating"></div>
+      </div>
+      `
+      gamesContainer.innerHTML += gameContent;
+    })
+  }
+
+  formContainer.addEventListener('input', () => sortGames(games));
 }
-
-const formContainer = document.querySelector('.form-container');
-let gamesContainer = document.querySelector('.games-container');
-
-function displayGames(games) {
-  gamesContainer.textContent = "";
-  
-  games.forEach(game => {
-    
-    const gameContent = `
-    <div class="game-container">
-    <img src="${game.imgURL}">
-    <div class="rating">68</div>
-    </div>
-    `
-    gamesContainer.innerHTML += gameContent;
-  })
-}
-
-function sortGamesByName(games) {
-  return games.sort((a, b) => {
-    let nameA = a.name.toUpperCase();
-    let nameB = b.name.toUpperCase();
-    return nameA > nameB ? 1 : -1;
-  })
-}
-
-function sortGamesByYear(games) {
-  return games.sort((a, b) => {
-    return a.releaseDate > b.releaseDate ? -1 : 1;
-  })
-}
-
-function filterByMulti(games) {
-  return games.filter(game => game.multiplayer);
-}
-
-formContainer.addEventListener('input', (e) => {
-  let val = e.target.value;
-  if (val === 'releaseDate') return displayGames(sortGamesByYear(GAMESSPLICE));
-  if (val === 'name' || val === 'all') return displayGames(sortGamesByName(GAMESSPLICE));
-  if (val === 'multi') return displayGames(filterByMulti(GAMESSPLICE));
-});
-
 
   // TODO
   // Collect genres
   // Collet ratings from metacritic
   // Generate random ratings and update color
   // Create loading animation?
-  
+
   //SORT
   // Year (ascending and descending)
   // Name (ascending and descending)
-  
+
   // FILTER
   // Multiplayer
-  
+
   // SEARCH
